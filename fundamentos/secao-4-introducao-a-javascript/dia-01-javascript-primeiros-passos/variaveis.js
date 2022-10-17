f3b7e07ec28652931421c189cf772ff34edef99c
araujo-marcio <araujo.marcio@pm.me>
2022-10-17 13:23:09 -0300

    Adiciona um cabeçalho na página contendo o título Soco a 80km/h: Conheça o Stomatopoda.

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-01-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,20 @@
+/* Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+Crie uma variável let chamada birthYear e atribua a ela o ano em que você nasceu. */
+let myName = 'Marcio';
+const birthCity = 'Rio de janeiro';
+let birthYear = 1989;
+
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+// console.log(myName, birthCity, birthYear);
+
+/* Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece! */
+birthYear=2030;
+// console.log(birthYear);
+
+/* Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? ??
+ */
+birthCity='Itaguaí';
+console.log(birthYear);
+
+// Apresenta erro, pois a variável birthCity está definida como const.
