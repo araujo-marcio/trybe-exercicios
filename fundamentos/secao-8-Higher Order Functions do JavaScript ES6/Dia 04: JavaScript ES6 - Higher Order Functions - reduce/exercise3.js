ebb7e19ee347f87eb4548141c45736ac0d61294a
araujo-marcio <araujo.marcio@pm.me>
2022-10-29 18:39:48 -0300

    Entrega do 3º exercicio
    Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.

---
+++ fundamentos/secao-8-Higher Order Functions do JavaScript ES6/Dia 04: JavaScript ES6 - Higher Order Functions - reduce/exercise3.js
@@ -0,0 +1,12 @@
+const {books} = require('./libraryData')
+
+const expectedResult = 43;
+
+function averageAge() {
+  const mediaBruta = books.reduce((acc, book) =>
+  acc += (book.releaseYear - book.author.birthYear) / books.length, 0);
+  const media = mediaBruta.toFixed();
+  return media;
+}
+
+console.log(averageAge());
