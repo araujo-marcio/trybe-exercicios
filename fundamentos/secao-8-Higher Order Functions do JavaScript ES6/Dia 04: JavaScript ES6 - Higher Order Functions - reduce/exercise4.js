5e0b6dbc3dd3c6f778420d0d325edf16ed68dba0
araujo-marcio <araujo.marcio@pm.me>
2022-10-30 13:50:50 -0300

    Entrega do 4º exercicio
    Encontre o livro com o maior nome.

---
+++ fundamentos/secao-8-Higher Order Functions do JavaScript ES6/Dia 04: JavaScript ES6 - Higher Order Functions - reduce/exercise4.js
@@ -0,0 +1,23 @@
+const {books} = require('./libraryData');
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  // escreva seu código aqui
+  return books.reduce((acc, curr) => { 
+    if(acc.name.length > curr.name.length) {
+      return acc;
+    }
+  })
+}
+
+console.log(longestNamedBook());
