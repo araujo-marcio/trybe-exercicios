e550c6dbf3a34adaa426c9c89461b0277e4cbcd1
Marcio Araujo <araujo.marcio@pm.me>
2023-02-17 11:09:26 -0300

    começando o exercício

---
+++ trybeMail/trybe-mail/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Inbox from './page/Inbox'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>TrybeMail </h1>;
+      </header>
+      <Inbox />
+    </div>
+  );
+}
+
+export default App;
